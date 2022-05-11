bc6854ffdb19909ec7307fb39c6c76c41aec7a42
kulak22 <105131387+kulak22@users.noreply.github.com>
2022-05-11 23:40:35 +0300

    Create matrix.cpp

---
+++ lab4/matrix.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include "matrix.h"
+
+using namespace std;
+matrix::matrix() {  // Конструктор по умолчанию 
+	this->a11 = 0;
+	this->a12 = 0;
+	this->a21 = 0;
+	this->a22 = 0;
+	this->detA = 0;
+}
+matrix::matrix(int a11, int a12, int a21, int a22) {  // Конструктор с параметрами
+	this->a11 = a11;
+	this->a12 = a12;
+	this->a21 = a21;
+	this->a22 = a22;
+	this->det(this->a11, this->a12 , this->a21, this->a22);
+}
+void matrix::Print() {
+	cout << endl;
+	cout << "\t" << a11 << "\t" << a12 << endl;
+	cout << "\t" << a21 << "\t" << a22 << endl;
+	cout << "detA = : " << detA << endl;
+}
+void matrix::det(int a11, int a12, int a21, int a22) {  // Функция которая считает определитель
+	this->detA = a11 * a22 - a12 * a21;
+}
+matrix matrix::operator = (const matrix & other){  // Оператор присваивания
+	this->a11 = other.a11;
+	this->a12 = other.a12;
+	this->a21 = other.a21;
+	this->a22 = other.a22;
+	this->detA = other.detA;
+	return *this;
+}
+matrix matrix::operator- (const matrix& other) {  // Оператор вычитания
+	matrix temp;
+	temp.a11 = this->a11 - other.a11;
+	temp.a12 = this->a12 - other.a12;
+	temp.a21 = this->a21 - other.a21;
+	temp.a22 = this->a22 - other.a22;
+	temp.det(temp.a11, temp.a12, temp.a21, temp.a22);
+	return temp;
+}
+matrix operator*= (matrix& other, int number) {  // Произвидение матрицы на число
+	other.a11 *=number;
+	other.a12 *=number;
+	other.a21 *=number;
+	other.a22 *= number;
+	other.det(other.a11, other.a12, other.a21, other.a22);
+	return other;
+}
+matrix operator-- (matrix& other) {  //  Префиксная ( меняет до) --a
+	other.a11--;
+	other.a12--;
+	other.a21--;
+	other.a22--;
+	other.det(other.a11, other.a12, other.a21, other.a22);  // Считаем определитель 
+	return other;
+}
+matrix operator--(matrix& other, int number) {  //  Постфиксная ( меняет после ) a--
+	matrix temp(other);
+	other.a11--;
+	other.a12--;
+	other.a21--;
+	other.a22--;
+	other.det(other.a11, other.a12, other.a21, other.a22);  // Считаем определитель 
+	return temp;
+}
