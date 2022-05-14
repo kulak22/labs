1b35159d6c5e7e8dcddd6900f88df212688ee98a
kulak22 <105131387+kulak22@users.noreply.github.com>
2022-05-14 20:36:46 +0300

    Create vector.h

---
+++ lab7/vector.h
@@ -0,0 +1,31 @@
+#ifndef LAB_7_2_CEM_VECTOR_H
+#define LAB_7_2_CEM_VECTOR_H
+#include <iostream> 
+
+using namespace std;
+const int MAX_SIZE = 20;
+
+
+class Vector {
+	int size;
+	int* arr;
+public:
+	Vector() { // Конструктор по умолчанию
+		size = 0;
+		arr = 0;
+	}
+	Vector(int s); // Конструктор с параметрами
+	Vector(int s, int* mas); // Конструктор с параметрами
+	Vector(const Vector& op1);
+	~Vector(); // Деструктор
+	const Vector& operator =(const Vector& op1);
+	int operator[](int i);  // Любой элемент массива
+	int operator ()(int s);
+	Vector operator -(int n);  // Удаляет n последних элементов
+	Vector operator +(int N);  // Удаляет n последних элементов
+	friend ostream& operator<<(ostream& out, const Vector& op1);
+	friend istream& operator>>(istream& in, Vector& op1);
+};
+
+
+#endif //LAB_7_2_CEM_VECTOR_H
