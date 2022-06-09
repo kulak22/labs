1152e77df7011fc8114778bee478d4366ca02e91
kulak22 <105131387+kulak22@users.noreply.github.com>
2022-06-09 12:00:55 +0300

    Update rightangled.h

--- lab5/rightangled.h
+++ lab5/rightangled.h
@@ -15,9 +15,9 @@ public:
 	RightAngled(); // Конструктор за замовчуванням
 	RightAngled(int a, int b, float c, int s); // Конструктор з параметрами
 	~RightAngled(); // Деструктор
-	RightAngled& operator=(const RightAngled& op1);
-	friend istream& operator>>(istream& in, RightAngled& op1);
-	friend ostream& operator<<(ostream& out, const  RightAngled& op1);
+	RightAngled& operator=(const RightAngled& op1);	// Оператор присвоєння
+	friend istream& operator>>(istream& in, RightAngled& op1);	// Оператор введення
+	friend ostream& operator<<(ostream& out, const  RightAngled& op1);	// Оператор виведення
 };
 
 #endif //LAB_5_2_CEM_RIGHTANGLED_H
